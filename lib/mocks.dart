b60c41a4fa87733cff5080cdf49b36ee249d4fac
Vanya <i.savchenko@ukrpay.net>
2021-06-22 15:56:31 +0300

    task 4.10.1

--- lib/mocks.dart
+++ lib/mocks.dart
@@ -16,7 +16,7 @@ final List mocks = [
     lat: 30.548504,
     lon: 1.2222,
     url:
-        "http://thenewcamera.com/wp-content/uploads/2019/09/Fuji-X-A7-sample-image-1.jpg",
+        "https://cdn.turkishairlines.com/m/4118b6df9b5d7df7/original/Travel-Guide-of-Kiev-via-Turkish-Airlines.jpg",
     details:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     type: "Парк",
@@ -26,7 +26,7 @@ final List mocks = [
     lat: 50.456206,
     lon: 30.548504,
     url:
-        "http://thenewcamera.com/wp-content/uploads/2019/09/Fuji-X-A7-sample-image-1.jpg",
+        "https://cdn2.civitatis.com/ucrania/kiev/free-tour-kiev.jpg",
     details:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     type: "Культурное место",
