6b6a2be3a217ed3e26ab1ed05271d47c1041a608
Vanya <i.savchenko@ukrpay.net>
2021-10-18 10:43:17 +0300

    task 7.4 EventLoop

--- lib/ui/screens/SplashScreen.dart
+++ lib/ui/screens/SplashScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:places/ui/res/assets.dart';
@@ -11,6 +12,7 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  List<String> list = List<String>.generate(100000, (index) => "gnirts $index");
   late Future isInitialized;
 
   void _navigateToNext() async {
@@ -18,10 +20,44 @@ class _SplashScreenState extends State<SplashScreen> {
         Duration(seconds: 2), () => print("Переход на следующий экран"));
   }
 
+  List<String> reverseStr(String from) {
+    print("started $from - " + "${DateTime.now()}");
+    List<String> reverseArr = [];
+    for (int i = 0; i < list.length; i++) {
+      setState(() {
+        reverseArr.add(list[i].split('').reversed.join(''));
+      });
+    }
+    print("end $from - " + "${DateTime.now()}");
+    return reverseArr;
+  }
+
+  void syncFunc() {
+    final _arr = reverseStr("syncFunc");
+    print(_arr);
+  }
+
+  void futureFunc() {
+    List<String> arr = [];
+    Future(() {
+      arr = reverseStr("futureFunc");
+    }).then((_) {
+      print(arr);
+    });
+  }
+
+  void isolateFunc() async {
+    final arr = await compute(reverseIsolate, list);
+    print(arr);
+  }
+
   @override
   void initState() {
     super.initState();
-    _navigateToNext();
+    syncFunc();
+    futureFunc();
+    isolateFunc();
+    // _navigateToNext();
   }
 
   @override
@@ -45,4 +81,15 @@ class _SplashScreenState extends State<SplashScreen> {
       ),
     );
   }
+}
+
+
+List<String> reverseIsolate(List<String> list) {
+  print("started isolateFunc - " + "${DateTime.now()}");
+  List<String> reverseArr = [];
+  for (int i = 0; i < list.length; i++) {
+    reverseArr.add(list[i].split('').reversed.join(''));
+  }
+  print("end isolateFunc - " + "${DateTime.now()}");
+  return reverseArr;
 }
